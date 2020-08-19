94b6f5fff8a9cde6e40d41bb12498df0a7557119
Andrew Barna <abarna@gmail.com>
2020-08-19 15:53:17 -0700

    make packaging better

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
 setup(
-    use_scm_version={
-        "write_to": "cchdo/params/_version.py",
-        "write_to_template": 'version = "{version}"\n',
-    }
+    use_scm_version=True
 )
