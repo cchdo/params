733ca2e8b747ff303b8fefbd78da062fdf000256
Andrew Barna <abarna@gmail.com>
2022-06-30 23:06:51 +0000

    Add Delta 30SI parameter

--- cchdo/params/params.sqlite3.sql
+++ cchdo/params/params.sqlite3.sql
@@ -5349,6 +5349,7 @@ INSERT INTO "ex_params" VALUES('CDOM412',NULL,'Attenuation coefficient of the sa
 INSERT INTO "ex_params" VALUES('CDOM443',NULL,'Attenuation coefficient of the sample at 442nm',NULL,NULL,'sample','decimal','woce_discrete',0,69.05);
 INSERT INTO "ex_params" VALUES('CDOM490',NULL,'Attenuation coefficient of the sample at 490nm',NULL,NULL,'sample','decimal','woce_discrete',0,69.06);
 INSERT INTO "ex_params" VALUES('CDOM555',NULL,'Attenuation coefficient of the sample at 555nm',NULL,NULL,'sample','decimal','woce_discrete',0,69.07);
+INSERT INTO "ex_params" VALUES('DELSI30',NULL,'Enrichment of the 30Si/28Si isotopic ratio in silica relative to NBS28',NULL,NULL,'sample','decimal','woce_discrete',0,60.2);
 CREATE TABLE ex_units (
 	id INTEGER NOT NULL, 
 	whp_unit VARCHAR, 
@@ -5790,4 +5791,5 @@ INSERT INTO "whp_names" VALUES('CDOM412','/METER','volume_beam_attenuation_coeff
 INSERT INTO "whp_names" VALUES('CDOM443','/METER','volume_beam_attenuation_coefficient_of_radiative_flux_in_sea_water','cdom443',NULL,NULL,NULL,NULL,NULL,9,4,443.0,NULL,NULL,NULL,'cdom');
 INSERT INTO "whp_names" VALUES('CDOM490','/METER','volume_beam_attenuation_coefficient_of_radiative_flux_in_sea_water','cdom490',NULL,NULL,NULL,NULL,NULL,9,4,490.0,NULL,NULL,NULL,'cdom');
 INSERT INTO "whp_names" VALUES('CDOM555','/METER','volume_beam_attenuation_coefficient_of_radiative_flux_in_sea_water','cdom555',NULL,NULL,NULL,NULL,NULL,9,4,555.0,NULL,NULL,NULL,'cdom');
+INSERT INTO "whp_names" VALUES('DELSI30','/MILLE',NULL,'delsi30',NULL,NULL,NULL,NULL,NULL,9,2,NULL,NULL,NULL,NULL,NULL);
 COMMIT;
