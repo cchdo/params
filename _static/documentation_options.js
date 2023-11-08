60ea4a1bae673906d84f09fac6217993c3bfa196
DocOtak <DocOtak@users.noreply.github.com>
2023-11-08 21:52:14 +0000

    deploy: 478e9ce4bed5be993bf11f038e2d1a67e394d66b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2023.9.1.dev5+g0fbe5eb',
+    VERSION: '2023.9.1.dev6+g478e9ce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
