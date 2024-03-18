b33c2e0f9d13a83d33412483703484de86e9df7a
DocOtak <DocOtak@users.noreply.github.com>
2024-03-18 23:36:18 +0000

    deploy: b639af1ec793a0dfb456c408280b8ded999d4437

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2023.11.1.dev4+gb48a088',
+    VERSION: '2023.11.1.dev6+gb639af1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
